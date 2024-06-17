b4ba0fd454f9b9e24b3540fd19ff756977ca595d
John Knotts <jhnnyk@gmail.com>
2024-06-17 16:28:03 -0600

    query parameters

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,15 @@
+import { useLocation } from "react-router-dom"
+
 export default function Contact() {
+  const queryString = useLocation().search
+  console.log(queryString)
+
+  const queryParams = new URLSearchParams(queryString)
+  const name = queryParams.get('name')
+
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>Hey {name}, contact us here...</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ipsum, doloribus maxime architecto ea minima recusandae ullam magni saepe. Unde natus fugit dolorum praesentium vero accusamus ipsam. Sint, earum necessitatibus.</p>
     </div>
   )
