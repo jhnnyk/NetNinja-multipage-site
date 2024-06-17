6846fb3d3d55ef768ef8a8dca810efaa6f62ee05
John Knotts <jhnnyk@gmail.com>
2024-06-17 16:15:30 -0600

    programatic redirects

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,10 +1,23 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { useFetch } from "../hooks/useFetch"
+import { useEffect } from "react"
 
 export default function Article() {
   const { id } = useParams()
   const url = 'http://localhost:3000/articles/' + id
   const { data: article, isPending, error } = useFetch(url)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    // check for error because useEffect always runs once 
+    // when the component is first loaded. Then because it is in the 
+    // dependencies, this will run again if an error gets set.
+    if (error) { 
+      setTimeout(() => {
+        navigate('/')
+      }, 2000);
+    }
+  }, [error, navigate])
 
   return (
     <div>
